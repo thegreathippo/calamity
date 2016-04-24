2b98bc2647f84df4b0fd7c8b946a0593c8cce2cd
thegreathippo <thegreathippo@gmail.com>
2016-04-24 02:27:33 -0400

    Update cycle.py

--- ecp/cycle.py
+++ ecp/cycle.py
@@ -1,28 +1,27 @@
 from ._entityindex import EntityIndex
 from ._registry import Registry
-import blinker
+import messages
 
 
 class Cycle(Registry):
-  def __init__(self):
-    self.index = EntityIndex()
-    self.running = True
-    blinker.signal("start").connect(self.start)
-    blinker.signal("stop").connect(self.stop)
-    self.start()
+    def __init__(self):
+        self.index = EntityIndex()
+        self.running = True
+        messages.requests.start_cycle.connect(self.start)
+        messages.requests.end_cycle.connect(self.end)
 
-  def start(self, e=None):
-    blinker.signal("starting").send(self)
-    while self.running:
-      blinker.signal("setting up").send(self)
-      self.tick()
-      blinker.signal("tearing down").send(self)
-    blinker.signal("stopping").send(self)
+    def start(self, e=None):
+        messages.events.start_cycle.send(self)
+        while self.running:
+            messages.events.before_tick.send(self)
+            self.tick()
+            messages.events.after_tick.send(self)
+        messages.events.end_cycle.send(self)
+
+    def tick(self):
+        for process in self.processes:
+            process.setup()
   
-  def tick(self):
-    for process in self.processes:
-      process.setup()
-  
-  def stop(self, e=None):
-    self.running = False
+    def stop(self, e=None):
+        self.running = False
   
